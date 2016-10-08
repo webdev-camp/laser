8c7cce93ba9c5ea9cdeed11c22c59ad320b9cb8a
Székely Zita <zita.szekely@gmail.com>
2016-10-08 17:28:49 +0300

    Adds controller, view and path for pages 'about, faq and ruby for newbies'

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,10 @@
 class MainController < ApplicationController
   def home
-
+  end
+  def about
+  end
+  def faq
+  end
+  def ruby_for_newbies
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :laser_gems
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "main#home"
+    get '/about', to: 'main#about', as: 'about'
+    get '/faq', to: 'main#faq', as: 'faq'
+    get '/ruby_for_newbies', to: 'main#ruby_for_newbies', as: 'ruby_for_newbies'
+
 end
