2dd08bbc4fa3bf9334d4e829b03f957cfcc5c83a
Alice <amyalicedee@gmail.com>
2016-10-10 00:43:28 +0300

    Adds GemDependency model, migration, associations and validations

---
+++ app/models/gem_dependency.rb
@@ -0,0 +1,16 @@
+class GemDependency < ApplicationRecord
+  belongs_to :laser_gem
+  belongs_to :dependency,
+    class_name: "LaserGem"
+  
+  # A LaserGem cannot have the same dependency LaserGem added twice
+  # Note that this prevents a gem with the same name being added as a dependency twice EVEN IF the vesion numbers are different.
+  validates_uniqueness_of :laser_gem_id, 
+    scope: :dependency_id
+
+  # A GemDependency must have both a LaserGem AND a dependency LaserGem
+  validates :laser_gem, :dependency, 
+    presence: true
+
+  validates :version, presence: true
+end
--- app/models/laser_gem.rb
+++ app/models/laser_gem.rb
@@ -1,2 +1,26 @@
 class LaserGem < ApplicationRecord
+  has_many :gem_dependencies
+
+  #
+  # Add a gem as a dependency of this one.
+  #
+  def register_dependency(dep, version:)
+    GemDependency.create!(
+      laser_gem: self,
+      dependency: dep,
+      version: version,
+    )
+    self.reload
+  end
+
+  #
+  # Remove a dependency
+  #
+  def remove_dependency(dep)
+    GemDependency.destroy_all(
+      laser_gem: self,
+      dependency: dep,
+    )
+    self.reload
+  end
 end
