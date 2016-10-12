81e6414b151de7a96b9e977b3658a9770038a657
Alice <amyalicedee@gmail.com>
2016-10-12 14:14:26 +0300

    Adds tests for register_ and remove_ dependency functions in LaserGem model

--- spec/models/laser_gem_spec.rb
+++ spec/models/laser_gem_spec.rb
@@ -1,4 +1,3 @@
-
 RSpec.describe LaserGem, type: :model do
   it "has working factory" do
     laser_gem = build :laser_gem
@@ -18,5 +17,25 @@ RSpec.describe LaserGem, type: :model do
   it "validates high values for length" do
     laser_gem = build :laser_gem, name: "hstglsnfmndvbdkjsdkfjdvjdfjdvkdjkkdjfkj"
     expect(laser_gem.save).to be false
+  end
+
+  describe "#register_dependency" do
+    it "creates a gem dependency" do
+      laser_gem = create :laser_gem
+      laser_gem2 = create :laser_gem
+      laser_gem.register_dependency(laser_gem2, "1.0.0")
+      expect(laser_gem.gem_dependencies.map(&:dependency)).to eq [laser_gem2]
+    end
+  end
+
+  describe "#remove_dependency" do
+    it "deletes a gem dependency" do
+      laser_gem = create :laser_gem
+      laser_gem2 = create :laser_gem
+      laser_gem.register_dependency(laser_gem2, "1.0.0")
+      expect(laser_gem.gem_dependencies.map(&:dependency)).to eq [laser_gem2]
+      laser_gem.remove_dependency(laser_gem2)
+      expect(laser_gem.gem_dependencies.map(&:dependency)).to eq []
+    end
   end
 end
