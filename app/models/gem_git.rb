bbf25d3ed323c40779b1cdf651d63cab7a5bc00e
Patricia Buchner Santos <pbuchner.santos@gmail.com>
2016-10-12 12:37:28 +0300

    Adds gem-git spec files and factory

--- app/models/gem_git.rb
+++ app/models/gem_git.rb
@@ -1,2 +1,10 @@
 class GemGit < ApplicationRecord
+  validates :name, length: {in: 2..30}
+  validates :homepage, presence: true
+  validates :last_commit, presence: true
+  validates :forks_count, presence: true
+  validates :stargazers_count, presence: true
+  validates :watchers_count, presence: true
+  validates :open_issues_count, presence: true
+
 end
