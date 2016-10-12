5f5433ed6649182e5d4d7b4d22b4ff5504d7077a
Patricia Buchner Santos <pbuchner.santos@gmail.com>
2016-10-12 12:36:55 +0300

    Adds gem-spec spec files and factory with a different name to avoid conflict

--- app/models/gem_spec.rb
+++ app/models/gem_spec.rb
@@ -1,2 +1,9 @@
 class GemSpec < ApplicationRecord
+  validates :name, length: { in: 2..30}
+  validates :info, presence: true
+  validates :current_version, presence: true
+  validates :current_version_downloads, presence: true
+  validates :total_downloads, presence: true
+  validates :rubygem_uri, presence: true
+  validates :documentation_uri, presence: true
 end
--- spec/models/gem_spec_spec.rb
+++ spec/models/gem_spec_spec.rb
@@ -1,4 +1,42 @@
 
 RSpec.describe GemSpec, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has working factory" do
+    gem_spec = build :gem_spec
+    expect(gem_spec.save).to be true
+  end
+
+  it "checks name attribute" do
+    gem_spec = build :gem_spec, name: ""
+    expect(gem_spec.save).to be false
+  end
+
+  it "checks info attribute" do
+    gem_spec = build :gem_spec, info: ""
+    expect(gem_spec.save).to be false
+  end
+
+  it "checks current_version attribute" do
+    gem_spec = build :gem_spec, current_version: ""
+    expect(gem_spec.save).to be false
+  end
+
+  it "checks current_version_downloads attribute" do
+    gem_spec = build :gem_spec, current_version_downloads: nil
+    expect(gem_spec.save).to be false
+  end
+
+  it "checks total_downloads attribute" do
+    gem_spec = build :gem_spec, total_downloads: nil
+    expect(gem_spec.save).to be false
+  end
+
+  it "checks rubygem_uri attribute" do
+    gem_spec = build :gem_spec, rubygem_uri: ""
+    expect(gem_spec.save).to be false
+  end
+
+  it "checks documentation_uri attribute" do
+    gem_spec = build :gem_spec, documentation_uri: ""
+    expect(gem_spec.save).to be false
+  end
 end
