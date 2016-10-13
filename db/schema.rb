3bb27687f5e5a028cf1fc92ed35fd40e48f7db5d
Székely Zita <zita.szekely@gmail.com>
2016-10-13 16:29:04 +0300

    Adds comment model with factory and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013092659) do
+ActiveRecord::Schema.define(version: 20161013132033) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "gem_dependencies", force: :cascade do |t|
     t.integer  "laser_gem_id"
