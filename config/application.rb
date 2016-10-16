00116c6f30c65e86272268d4d0412689399eab69
Alice <amyalicedee@gmail.com>
2016-10-16 05:37:16 +0300

    Add app/lib to autoload paths

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Laser
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
