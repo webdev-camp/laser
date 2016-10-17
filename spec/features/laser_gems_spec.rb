133322f1f8348c51aa8422afaca7afbdb05f041d
Alice <amyalicedee@gmail.com>
2016-10-17 19:07:32 +0300

    Fixes #11

--- spec/features/laser_gems_spec.rb
+++ spec/features/laser_gems_spec.rb
@@ -7,12 +7,14 @@ RSpec.describe "LaserGems" do
 
   it "index with item works" do
     gem = create :laser_gem
+    create :gem_spec, laser_gem_id: gem.id
     visit laser_gems_path
     expect(page.status_code).to be 200
   end
 
-  it "index with item works" do
+  it "show for item works" do
     gem = create :laser_gem
+    create :gem_spec, laser_gem_id: gem.id
     visit laser_gems_path
     click_link gem.name
     expect(page.status_code).to be 200
