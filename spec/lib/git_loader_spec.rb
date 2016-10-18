b3dc4ff94d2b11bc906a02a4dd4f36242c2373cf
Székely Zita <zita.szekely@gmail.com>
2016-10-18 16:07:27 +0300

    Use full name attribute for gem_git

--- spec/lib/git_loader_spec.rb
+++ spec/lib/git_loader_spec.rb
@@ -3,14 +3,14 @@ RSpec.describe GitLoader do
     expect(GitLoader.new).not_to be nil
   end
 
-  it "saves an instance of GemGit for each laser_gem", :ci => true do
+  it "saves an instance of GemGit for each laser_gem" do
      loader = GitLoader.new
-     laser_gem = LaserGem.create!(name: "rails")
+     laser_gem = create :laser_gem, name: "rails"
      create :gem_spec,
          name: "rails",
          source_code_uri: "http://github.com/rails/rails",
          laser_gem_id: laser_gem.id
      loader.populate_data(laser_gem)
-     expect(GemGit.exists?(name: "rails")).to be true
+     expect(GemGit.exists?(name: "rails/rails")).to be true
   end
 end
