e1c11735537b36963f564c7c74754981e2355300
alexanderspace100 <alexanderspace100@gmail.com>
2016-10-22 15:24:23 +0300

    Adds dependecies and dependents to show.Closes #8.

--- spec/factories/laser_gems.rb
+++ spec/factories/laser_gems.rb
@@ -7,15 +7,15 @@ FactoryGirl.define do
        after(:create) do |laser_gem, evaluator|
          create :gem_spec, laser_gem: laser_gem
        end
-     end
-     factory :laser_gem_with_dependencies do
-       after(:create) do |laser_gem, evaluator|
-         create_list :gem_dependency, 5, laser_gem: laser_gem
+       factory :laser_gem_with_dependencies do
+         after(:create) do |laser_gem, evaluator|
+           create_list :gem_dependency, 5, laser_gem: laser_gem
+         end
        end
-     end
-     factory :laser_gem_with_dependents do
-       after(:create) do |laser_gem, evaluator|
-         create_list :gem_dependency, 5, dependency: laser_gem
+       factory :laser_gem_with_dependents do
+         after(:create) do |laser_gem, evaluator|
+           create_list :gem_dependency, 5, dependency: laser_gem
+         end
        end
      end
       factory :laser_gem_with_gem_git do
--- spec/features/laser_show_spec.rb
+++ spec/features/laser_show_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe "LaserGemShowPage" do
+RSpec.describe "LaserGemsShow" do
 
   def add_tag tag
     laser_gem = create :laser_gem_with_spec
@@ -21,5 +21,23 @@ RSpec.describe "LaserGemShowPage" do
     add_tag "thfsf gem"
     expect(page).to have_text("Please insert a valid tag")
     expect(page).not_to have_content("thfsf gem")
+  end
+
+  it "shows the laser gem name" do
+    laser_gem = create :laser_gem_with_spec
+    visit laser_gem_path(laser_gem.name)
+    expect(page).to have_text(laser_gem.name)
+  end
+
+  it "shows the laser gem dependencies" do
+    laser_gem = create :laser_gem_with_dependencies
+    visit laser_gem_path(laser_gem.name)
+    expect(page).to have_text(laser_gem.dependencies.first.name)
+  end
+
+  it "shows the laser gem dependents" do
+    laser_gem = create :laser_gem_with_dependents
+    visit laser_gem_path(laser_gem.name)
+    expect(page).to have_text(laser_gem.dependents.first.name)
   end
 end
