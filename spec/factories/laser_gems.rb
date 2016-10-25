b5854eedd4ee2c32584615c5ef59048127dee882
Alice <amyalicedee@gmail.com>
2016-10-25 14:19:21 +0300

    Adds tests for factories and models ownerships associations

--- spec/factories/laser_gems.rb
+++ spec/factories/laser_gems.rb
@@ -1,33 +1,39 @@
 FactoryGirl.define do
   factory :laser_gem do
     sequence :name do |n|
-       "Ruby#{n}"
-     end
-     factory :laser_gem_with_spec do
-       after(:create) do |laser_gem, evaluator|
-         create :gem_spec, laser_gem: laser_gem
-       end
-     end
-     factory :laser_gem_with_dependencies do
-       after(:create) do |laser_gem, evaluator|
-         create_list :gem_dependency, 5, laser_gem: laser_gem
-       end
-     end
-     factory :laser_gem_with_dependents do
-       after(:create) do |laser_gem, evaluator|
-         create_list :gem_dependency, 5, dependency: laser_gem
-       end
-     end
-      factory :laser_gem_with_gem_git do
-        after(:create) do |laser_gem, evaluator|
-          create :gem_git, laser_gem: laser_gem
-        end
+      "Ruby#{n}"
+    end
+    factory :laser_gem_with_spec do
+      after(:create) do |laser_gem, evaluator|
+        create :gem_spec, laser_gem: laser_gem
       end
-      factory :laser_gem_with_source_code_uri do
-        after(:create) do |laser_gem, evaluator|
-          create :gem_spec, laser_gem: laser_gem,
-                             source_code_uri: "http://github.com/rails/rails"
-        end
+    end
+    factory :laser_gem_with_dependencies do
+      after(:create) do |laser_gem, evaluator|
+        create_list :gem_dependency, 5, laser_gem: laser_gem
       end
+    end
+    factory :laser_gem_with_dependents do
+      after(:create) do |laser_gem, evaluator|
+        create_list :gem_dependency, 5, dependency: laser_gem
+      end
+    end
+    factory :laser_gem_with_gem_git do
+      after(:create) do |laser_gem, evaluator|
+        create :gem_git, laser_gem: laser_gem
+      end
+    end
+    factory :laser_gem_with_source_code_uri do
+      after(:create) do |laser_gem, evaluator|
+        create :gem_spec, laser_gem: laser_gem,
+          source_code_uri: "http://github.com/rails/rails"
+      end
+    end
+
+    factory :laser_gem_with_ownership do
+      after(:create) do |laser_gem, evaluator|
+        create :ownership, laser_gem: laser_gem
+      end
+    end
   end
 end
--- spec/factories/ownerships.rb
+++ spec/factories/ownerships.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
   factory :ownership do
-    name "MyString"
-    email "alice@dee.com"
-    laser_gem { create :laser_gem }
+    sequence :email do |n|
+      "ruby#{n}@mail.com"
+    end
+    owner { create :user }
+    gem_spec
+    laser_gem
+    gem_git
+    role "owner"
   end
 end
