8b4598d5bcfdb4ef975d523ab0ed8e0e1bc818d6
alexanderspace100 <alexanderspace100@gmail.com>
2016-10-26 15:26:47 +0300

    Adds style to comments form.

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :comment do
     body "Lorem Ipsum has been the industry's standard dummy text ever since"
-    laser_gem { create :laser_gem }
-    user { create :user}
+    laser_gem
+    user 
   end
 end
