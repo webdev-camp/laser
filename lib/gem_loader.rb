e8371230a04d5f5af51a531dbdb9694835503374
Torsten Ruger <torsten@villataika.fi>
2016-10-31 15:51:35 +0200

    fixes for invalid gem data
    passes the run

--- lib/gem_loader.rb
+++ lib/gem_loader.rb
@@ -5,7 +5,9 @@ class GemLoader
   end
 
   def get_spec_from_api(gem_name)
-    @client.info(gem_name)
+    data = @client.info(gem_name)
+    return nil if data == "This rubygem could not be found."
+    return data
   end
 
   def get_owners_from_api(gem_name)
@@ -13,25 +15,25 @@ class GemLoader
   end
 
   def get_build_start_from_api(gem_name)
-    @client.versions(gem_name)
+    @client.versions(gem_name)[-1]
   end
 
   def fetch_and_create_gem_spec(laser_gem)
     gem_data = get_spec_from_api(laser_gem.name)
     return unless gem_data
-    first_version = get_build_start_from_api(laser_gem.name)
-    attribs = {}
-    spec_attributes.each  do |k,v|
-      attribs[k] = gem_data[v]
+    unless laser_gem.gem_spec
+      first_version = get_build_start_from_api(laser_gem.name)
+      attribs = {laser_gem_id: laser_gem.id, build_date: first_version["built_at"]}
+      spec_attributes.each  { |k,v| attribs[k] = gem_data[v]}
+      GemSpec.create!(attribs)
     end
-    GemSpec.find_or_create_by!(attribs.merge laser_gem_id: laser_gem.id, build_date: first_version[-1]["built_at"])
-    fetch_and_spec_deps(laser_gem, gem_data["dependencies"]["runtime"])
     fetch_owners(laser_gem)
+    fetch_and_spec_deps(laser_gem, gem_data["dependencies"]["runtime"] )
   end
 
   # create ownerships for each rubygems owner
   def fetch_owners(laser_gem)
-    return unless laser_gem.ownerships.where(["rubygem_owner = ?", true]).count == 0
+    return unless laser_gem.ownerships.where(rubygem_owner: true).empty?
     owner_array = get_owners_from_api(laser_gem.name)
     owner_array.each do |owner|
       gem_handle = owner["handle"]
@@ -39,16 +41,19 @@ class GemLoader
       ownership = Ownership.find_or_create_by!(laser_gem_id: laser_gem.id, email: email)
       ownership.update(rubygem_owner: true, gem_handle: gem_handle)
     end
-    # fetch_owners_for_deps(laser_gem)
     laser_gem.dependencies.each { |dep| fetch_owners(dep) }
   end
 
   def fetch_and_spec_deps(laser_gem, runtime_deps)
     runtime_deps.each do |gem_dep|
-      dep = gem_dep["name"]
+      dep_name = gem_dep["name"]
       ver = gem_dep["requirements"]
-      lg_dep = LaserGem.find_or_create_by!(name: dep)
-      laser_gem.register_dependency(lg_dep, ver) unless GemDependency.where("laser_gem_id = ? and dependency_id = ?", laser_gem.id, lg_dep.id).exists?
+      lg_dep = LaserGem.find_or_create_by!(name: dep_name)
+      begin
+        laser_gem.register_dependency(lg_dep, ver) unless GemDependency.where("laser_gem_id = ? and dependency_id = ?", laser_gem.id, lg_dep.id).exists?
+      rescue ActiveRecord::RecordInvalid => e
+        puts dep_name + " invalid " + e.message
+      end
       fetch_and_create_gem_spec(lg_dep) if lg_dep.gem_spec.nil?
     end
   end
--- lib/tag_loader.rb
+++ lib/tag_loader.rb
@@ -10,17 +10,18 @@ class TagLoader
 
   def load_tags
     @tags.each do |key, values|
-      sleep(5) 
-      puts key.to_s
+      next if key.length < 3
+      puts key.to_s + " tagloader"
       laser_gem = LaserGem.find_or_create_by!(name: key)
       add_tags_to_gem(laser_gem, values)
-      @gem_loader.fetch_and_create_gem_spec(laser_gem)
-      @git_loader.fetch_and_create_gem_git(laser_gem)
+      @gem_loader.fetch_and_create_gem_spec(laser_gem) unless laser_gem.gem_spec
+      @git_loader.fetch_and_create_gem_git(laser_gem)  unless laser_gem.gem_git
     end
   end
 
 private
   def add_tags_to_gem(laser_gem, values)
+    return unless laser_gem.tag_list.empty?
     laser_gem.tag_list.add(values.first)
     laser_gem.tag_list.add(values.last)
     laser_gem.save!
