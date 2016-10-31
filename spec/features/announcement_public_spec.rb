0d629c3c9b80c17211b6baa4c0755bdead8f26c9
Székely Zita <zita.szekely@gmail.com>
2016-10-31 14:04:33 +0200

    Adds one more test to the announcement public spec

--- spec/features/announcement_public_spec.rb
+++ spec/features/announcement_public_spec.rb
@@ -12,4 +12,9 @@ RSpec.describe "Announcements" do
     expect(page).to have_text announcement.title
   end
 
+  it "goes to log in page when not logged in" do
+    visit new_announcement_path
+    expect(page).to have_current_path(new_user_session_path)
+  end
+
 end
