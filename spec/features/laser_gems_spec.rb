7f8007a71b84c9cf0e04f6140da135e3f5469ad6
Székely Zita <zita.szekely@gmail.com>
2016-11-05 09:36:24 +0200

    Adds search functionality and tests

--- spec/features/laser_gems_spec.rb
+++ spec/features/laser_gems_spec.rb
@@ -19,4 +19,18 @@ RSpec.describe "LaserGems" do
     click_link gem.name
     expect(page.status_code).to be 200
   end
+
+  it "searches for gem stuff " do
+    laser_gem = create :laser_gem_with_spec
+    create :laser_gem_with_spec
+    visit laser_gems_path
+    # find and set the search input
+    page.fill_in 'q_name_cont', :with => laser_gem.name
+    # click the search button
+    page.find('input[name="commit"]').click
+    expect(page.status_code).to be 200
+    # check that there are some results
+    gem_elements = page.find_all('.gem_element')
+    expect(gem_elements.length).to be 1
+  end
 end
