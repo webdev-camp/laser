d1bce1a5da81e3e76974ba3bbf2be8308ff57458
Alice <amyalicedee@gmail.com>
2016-11-05 18:30:41 +0200

    Ranking: Add additional score methods

--- lib/ranking.rb
+++ lib/ranking.rb
@@ -1,24 +1,52 @@
-module Ranking
+class Ranking
+  COMMIT_ACTIVITY_BANDS = [1.week.ago, 3.months.ago, 6.months.ago, 1.year.ago]
+  RECENT_ACTIVITY_BANDS = [1.week.ago, 1.month.ago, 3.months.ago, 6.months.ago]
+  FORKS_BANDS = [5000, 500, 50, 1]
+  STARGAZERS_BANDS = [5000, 500, 50, 5]
+  WATCHERS_BANDS = [1000, 100, 10, 1]
 
-  def self.activity_score(laser_gem)
-    gem_git = GemGit.find_by(laser_gem_id: laser_gem.id)
-    if gem_git
-      commit_activity(laser_gem, gem_git.commit_dates.last)
-    end
+  def initialize(laser_gem)
+    @laser_gem = laser_gem
+    @gem_git = GemGit.find_by(laser_gem_id: @laser_gem.id)
+  end
+
+  def commit_activity_score
+    return 0 unless @gem_git
+    score_calculation(@gem_git.commit_dates.first, COMMIT_ACTIVITY_BANDS)
+  end
+
+  def recent_activity_score
+    return 0 unless @gem_git
+    score_calculation(@gem_git.last_commit, RECENT_ACTIVITY_BANDS)
+  end
+
+  def forks_score
+    return 0 unless @gem_git
+    score_calculation(@gem_git.forks_count, FORKS_BANDS)
+  end
+
+  def stargazers_score
+    return 0 unless @gem_git
+    score_calculation(@gem_git.stargazers_count, STARGAZERS_BANDS)
+  end
+
+  def watchers_score
+    return 0 unless @gem_git
+    score_calculation(@gem_git.watchers_count, WATCHERS_BANDS)
   end
 
   private
 
-  def self.commit_activity(laser_gem, commit_date)
-    if commit_date > 1.week.ago
+  def score_calculation(criterion, score_array)
+    if criterion > score_array[0]
       return 5
-    elsif commit_date > 1.month.ago
+    elsif criterion > score_array[1]
       return 4
-    elsif commit_date > 6.months.ago
+    elsif criterion > score_array[2]
       return 3
-    elsif commit_date > 1.year.ago
+    elsif criterion > score_array[3]
       return 2
-    elsif commit_date < 1.year.ago
+    else
       return 1
     end
   end
--- spec/lib/ranking_spec.rb
+++ spec/lib/ranking_spec.rb
@@ -1,11 +1,67 @@
 RSpec.describe Ranking do
-  describe "#activity_score" do
+  describe "#commit_activity_score" do
+    it "returns zero if there is no gem_git" do
+      laser_gem = LaserGem.create(name: "letmein")
+      expect(Ranking.new(laser_gem).commit_activity_score).to be 0
+    end
     it "ranks activity score 1 if latest commit was over a year ago" do
       laser_gem = LaserGem.create(name: "letmein")
       create :gem_git, 
-        commit_dates: [2.years.ago], 
+        commit_dates: [2.years.ago, 3.years.ago], 
         laser_gem: laser_gem
-      expect(Ranking.activity_score(laser_gem)).to be 1
+      expect(Ranking.new(laser_gem).commit_activity_score).to be 1
+    end
+  end
+  describe "#recent_activity_score" do
+    it "returns zero if there is no gem_git" do
+      laser_gem = LaserGem.create(name: "letmein")
+      expect(Ranking.new(laser_gem).recent_activity_score).to be 0
+    end
+    it "ranks recent activity score 5 if latest commit was less than a week ago" do
+      laser_gem = LaserGem.create(name: "rails")
+      create :gem_git, 
+        last_commit: 1.day.ago, 
+        laser_gem: laser_gem
+      expect(Ranking.new(laser_gem).recent_activity_score).to be 5
+    end
+  end
+  describe "#forks_score" do
+    it "returns zero if there is no gem_git" do
+      laser_gem = LaserGem.create(name: "letmein")
+      expect(Ranking.new(laser_gem).forks_score).to be 0
+    end
+    it "ranks forks score 3 if fork count is more than 50 but less than 500" do
+      laser_gem = LaserGem.create(name: "rails")
+      create :gem_git, 
+        forks_count: 100,
+        laser_gem: laser_gem
+      expect(Ranking.new(laser_gem).forks_score).to be 3
+    end
+  end
+  describe "#stargazers_score" do
+    it "returns zero if there is no gem_git" do
+      laser_gem = LaserGem.create(name: "letmein")
+      expect(Ranking.new(laser_gem).stargazers_score).to be 0
+    end
+    it "ranks stargazer score 3 if stargazer_count is more than 50 but less than 500" do
+      laser_gem = LaserGem.create(name: "rails")
+      create :gem_git, 
+        stargazers_count: 100,
+        laser_gem: laser_gem
+      expect(Ranking.new(laser_gem).stargazers_score).to be 3
+    end
+  end
+  describe "#watchers_score" do
+    it "returns zero if there is no gem_git" do
+      laser_gem = LaserGem.create(name: "letmein")
+      expect(Ranking.new(laser_gem).watchers_score).to be 0
+    end
+    it "ranks watchers score 1 if watchers_count is less than 1" do
+      laser_gem = LaserGem.create(name: "rails")
+      create :gem_git, 
+        watchers_count: 0,
+        laser_gem: laser_gem
+      expect(Ranking.new(laser_gem).watchers_score).to be 1
     end
   end
 end
