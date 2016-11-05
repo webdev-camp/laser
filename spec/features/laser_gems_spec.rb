fdebe5190e40c555f691a7419e23e673121e3ec9
alexanderspace100 <alexanderspace100@gmail.com>
2016-11-05 11:56:47 +0200

    Adds tests for related tags, popular tags, popular gems.

--- spec/features/laser_gems_spec.rb
+++ spec/features/laser_gems_spec.rb
@@ -1,14 +1,18 @@
 
 RSpec.describe "LaserGems" do
+  def create_gem
+    gem = create :laser_gem
+    create :gem_spec, laser_gem_id: gem.id
+    visit laser_gems_path
+  end
+
   it "gets index" do
     visit laser_gems_path
     expect(page.status_code).to be 200
   end
 
   it "index with item works" do
-    gem = create :laser_gem
-    create :gem_spec, laser_gem_id: gem.id
-    visit laser_gems_path
+    create_gem
     expect(page.status_code).to be 200
   end
 
@@ -18,5 +22,11 @@ RSpec.describe "LaserGems" do
     visit laser_gems_path
     click_link gem.name
     expect(page.status_code).to be 200
+  end
+
+  it "shows the related tags for laser gem" do
+    laser_gem = create :laser_gem_with_spec
+    visit laser_gems_path
+    expect(page).to have_text(laser_gem.tag_list)
   end
 end
