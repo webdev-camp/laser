a8d983fdcbcdbe66e8986d5ee3af96f89163ba87
Székely Zita <zita.szekely@gmail.com>
2016-11-05 10:06:53 +0200

    Corrects failed test

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -3,6 +3,6 @@ class Announcement < ApplicationRecord
   validates :title, length: { in: 5..30}, presence: true
   validates :body, length: {minimum: 10, maximum: 3000}, presence: true
 
-  belongs_to :user
+  belongs_to :user , required: true
 
 end
