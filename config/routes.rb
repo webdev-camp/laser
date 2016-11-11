8928038c4d5dffd88d059e540664710a908cfab3
Torsten Ruger <torsten@villataika.fi>
2016-11-11 12:21:55 +0200

    change routes to gem (from laser_gem)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :announcements, except: :destroy
-  match "laser_gems", to: "laser_gems#index" , via: [:get, :post]
-  post 'laser_gem/:name/add_tag', to: 'laser_gems#add_tag', as: :add_tag
-  post 'laser_gem/:name/add_comment', to: 'laser_gems#add_comment', as: :add_comment
-  get   "laser_gem/:name"  , to: "laser_gems#show" , as: :laser_gem
+  match "gems", to: "laser_gems#index" , via: [:get, :post] , as: :laser_gems
+  post 'gem/:name/add_tag', to: 'laser_gems#add_tag', as: :add_tag
+  post 'gem/:name/add_comment', to: 'laser_gems#add_comment', as: :add_comment
+  get   "gem/:name"  , to: "laser_gems#show" , as: :laser_gem
   get 'tags/:tag', to: 'laser_gems#index', as: :tag
 
   root to: "main#home"
