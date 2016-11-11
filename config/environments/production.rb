220c14d2ba2f130b372e6e7d733849e991245ffc
Torsten Ruger <torsten@villataika.fi>
2016-11-11 10:12:15 +0200

    securing the site
    the whole thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -68,7 +68,8 @@ Rails.application.configure do
         :authentication       => :login,
         :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = {  protocol: 'https',
+                                                host:     Rails.application.secrets.domain_name }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {  from: Rails.application.secrets.admin_email ,
