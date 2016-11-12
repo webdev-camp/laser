4802c57a1e9513b4ddc5adfea2b915781305655f
alexanderspace100 <alexanderspace100@gmail.com>
2016-11-12 12:39:58 +0200

    Adds tests checking the associations for ownerships.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe User, type: :model do
     user = build :user, name: ""
     expect(user.save).to be false
   end
+
+  it "checks that a new user has no ownerships" do
+    user = create :user
+    expect(user.ownerships.length).to be 0
+  end
+
+  it "verifies ownership associations" do
+    ownership = create :ownership
+    user = ownership.owner
+    expect(user.laser_gems.include?(ownership.laser_gem)).to be true
+  end
+
+  it "checks that an user has an ownership" do
+    ownership = create :ownership, owner: nil
+    user = create :user, email: ownership.email
+    user.connect_ownerships
+    expect(user.ownerships.length).to be 1
+    expect(user.laser_gems.length).to be 1
+  end
 end
