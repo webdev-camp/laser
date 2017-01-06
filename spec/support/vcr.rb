8863e57f5b37365b9dc1f7b2ec04b151c4ce61d8
Torsten Ruger <torsten@villataika.fi>
2017-01-06 19:47:46 +0200

    let codeclimate through the web mock

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,7 @@
 VCR.configure do |c|
-  #the directory where your cassettes will be saved
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+
+  c.ignore_hosts 'codeclimate.com' 
 end
