5669ff1378413806c59a447a73364141ace62159
Torsten Ruger <torsten@villataika.fi>
2017-01-07 12:21:59 +0200

    updating gems gem, changes all vcr data because of json instead of yaml requests

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,6 +2,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-
-  c.ignore_hosts 'codeclimate.com' 
+  c.default_cassette_options = {record: :new_episodes} #change to all to update the cassette data
+  c.ignore_hosts 'codeclimate.com'
 end
